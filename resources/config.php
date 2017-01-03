353b115701d4689e6770500c897f20d0e933cf90
Max Moreno <Rodney.Moreno14@gmail.com>
2017-01-03 16:03:54 -0600

    renamed:    index.html -> index.php 	modified:   resources/config.php

--- resources/config.php
+++ resources/config.php
@@ -3,7 +3,7 @@
 //Creating the neccesarry requriments for the database connection
 $db_host = 'localhost';
 $db_user = 'root';
-$db_pass = '352232';
+$db_pass = '352232max';
 $db_name = 'blog';
 
 ?>
